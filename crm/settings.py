f65607d47c1e14cddb7cac2f300dd82add299062
Abhav <goelabhav2002@gmail.com>
2023-07-22 04:52:45 +0530

    added postgres support

--- crm/settings.py
+++ crm/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'crm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'DEMO_TEST',
+        'USER':'postgres',
+        'PASSWORD':'goelabhav2002',
+        'HOST':'localhost',
+        'PORT':'5432'
+
+
     }
 }
 
